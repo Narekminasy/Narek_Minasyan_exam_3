39e628af067f4b7673b581ccb43d174d3732b99f
Narek Minasyan <narekminasyan52@gmail.com>
2026-06-04 09:34:18 +0400

    add film.js logic

---
+++ models/Film.js
@@ -0,0 +1,24 @@
+import {DataTypes,Model} from 'sequelize';
+import db from '../clients/db.sequelize.js';
+
+
+class Film extends Model {}
+
+Film.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    duration: DataTypes.INTEGER,
+},{
+    sequelize: db,
+    modelName: "films",
+    tableName: "films",
+    timestamps: true
+})
+
+export default Film;
